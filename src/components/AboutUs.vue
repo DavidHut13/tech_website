cca41741c90b9ff9821caf8ba4f78b12b310d4c9
Annah Isenberg <annah.isenberg@msionline.com>
2019-05-20 14:10:15 -0500

    make about-us section responsive

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -5,67 +5,83 @@
         <h2>ABOUT US</h2>
       </div>
       <v-layout justify-space-around row>
-        <v-flex align-self-center="true" xs1>
-          <v-icon size="34px">fas fa-star</v-icon>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">fas fa-star</v-icon>
+          </div>
+          <div>
+            <p class="bolded">Help Desk Services</p>
+            <p>badge accesses, employee terminations, phone issues, customer service</p>
+          </div>
         </v-flex>
-        <v-flex xs5>
-          <p class="bolded">Help Desk Services</p>
-          <p>badge accesses, employee terminations, phone issues, customer service</p>
-        </v-flex>
-        <v-flex align-self-center="true" offset-xs1 xs1>
-          <v-icon size="34px">fas fa-smile</v-icon>
-        </v-flex>
-        <v-flex xs5>
-          <p class="bolded">System Administrator</p>
-          <p>server patching, hacking & cyber attacks, assures our systems are current</p>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex align-self-center="true" xs1>
-          <v-icon size="34px">fas fa-network-wired</v-icon>
-        </v-flex>
-        <v-flex xs5>
-          <p class="bolded">Network Administrator</p>
-          <p>oversees firewall switches, cyber attacks, phone systems</p>
-        </v-flex>
-        <v-flex align-self-center="true" offset-xs1 xs1>
-          <v-icon size="34px">fas fa-wrench</v-icon>
-        </v-flex>
-        <v-flex xs5>
-          <p class="bolded">Development</p>
-          <p>full-stack development, can integrate with anybody on anything. Create websites, custom software</p>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">fas fa-smile</v-icon>
+          </div>
+          <div>
+            <p class="bolded">System Administrator</p>
+            <p>server patching, hacking & cyber attacks, assures our systems are current</p>
+          </div>
         </v-flex>
       </v-layout>
-      <v-layout row>
-        <v-flex align-self-center="true" xs1>
-          <v-icon size="34px">far fa-clock</v-icon>
+      <v-layout justify-space-around row>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">fas fa-network-wired</v-icon>
+          </div>
+          <div>
+            <p class="bolded">Network Administrator</p>
+            <p>oversees firewall switches, cyber attacks, phone systems</p>
+          </div>
         </v-flex>
-        <v-flex xs5>
-          <p class="bolded">Procurement Analyst</p>
-          <p>tracks the trends of profit and loss, funds, tracks all the money</p>
-        </v-flex>
-        <v-flex align-self-center="true" offset-xs1 xs1>
-          <v-icon size="34px">fas fa-phone</v-icon>
-        </v-flex>
-        <v-flex xs5>
-          <p class="bolded">Report Writer</p>
-          <p>does all kinds of custom reports and automates them if she can. SSRS specialist.</p>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">fas fa-wrench</v-icon>
+          </div>
+          <div>
+            <p class="bolded">Development</p>
+            <p>full-stack development, can integrate with anybody on anything. Create websites, custom software</p>
+          </div>
         </v-flex>
       </v-layout>
-      <v-layout row>
-        <v-flex align-self-center="true" xs1>
-          <v-icon size="34px">fas fa-laptop-code</v-icon>
+      <v-layout justify-space-around row>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">far fa-clock</v-icon>
+          </div>
+          <div>
+            <p class="bolded">Procurement Analyst</p>
+            <p>tracks the trends of profit and loss, funds, tracks all the money</p>
+          </div>
         </v-flex>
-        <v-flex xs5>
-          <p class="bolded">Database Administrator</p>
-          <p>oversees all the databases, keeps them efficient and streamlined</p>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">fas fa-phone</v-icon>
+          </div>
+          <div>
+            <p class="bolded">Report Writer</p>
+            <p>does all kinds of custom reports and automates them if she can. SSRS specialist.</p>
+          </div>
         </v-flex>
-        <v-flex align-self-center="true" offset-xs1 xs1>
-          <v-icon size="34px">fas fa-chart-line</v-icon>
+      </v-layout>
+      <v-layout justify-space-around row>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">fas fa-laptop-code</v-icon>
+          </div>
+          <div>
+            <p class="bolded">Database Administrator</p>
+            <p>oversees all the databases, keeps them efficient and streamlined</p>
+          </div>
         </v-flex>
-        <v-flex xs5>
-          <p class="bolded">Senior IT Project Manager</p>
-          <p>manages infrastructure and development</p>
+        <v-flex align-self-center="true" xs12 md6>
+          <div class="div-icon">
+            <v-icon size="34px">fas fa-chart-line</v-icon>
+          </div>
+          <div>
+            <p class="bolded">Senior IT Project Manager</p>
+            <p>manages infrastructure and development</p>
+          </div>
         </v-flex>
       </v-layout>
     </v-container>
@@ -93,6 +109,20 @@ export default {
     @media only screen and (max-width: 768px) {
       //for mobile phones
       width: 100%;
+    }
+
+    .layout {
+      @media only screen and (max-width: 768px) {
+        //for mobile phones
+        flex-direction: column;
+      }
+    }
+
+    .flex {
+      display: flex;
+      .div-icon {
+        align-self: center;
+      }
     }
   }
 
